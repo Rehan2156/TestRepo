de2d23909ecd7fea422fa15cfdccff1f14409abd
Rehan Shaikh <rehanshaikh2156@gmail.com>
2022-02-23 14:09:43 +0530

    Update soil.py

--- soil.py
+++ soil.py
@@ -7,9 +7,9 @@ GPIO.setup(channel, GPIO.IN)
  
 def callback(channel):
         if GPIO.input(channel):
-                print "Water Detected!"
+                print("No Water Detected!")
         else:
-                print "Water Detected!"
+                print("Water Detected!")
  
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(channel, callback) 
